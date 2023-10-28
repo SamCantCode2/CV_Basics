a74a448f04d450b0c18272e6dec33114da6a833b
Samhruth Ananthanarayanan <122143797+SamCantCode2@users.noreply.github.com>
2023-10-28 16:12:10 +0530

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread(r'C:\Users\Samhruth\Pictures\Saved Pictures\ForEncry.jpeg')
+img = cv.imread(r'')
 cv.imshow('Sudo', img)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
