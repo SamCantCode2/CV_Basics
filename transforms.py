740f4e1e7774f4b3ca464bf51138a550f1e545f9
Samhruth Ananthanarayanan <122143797+SamCantCode2@users.noreply.github.com>
2023-10-28 16:11:49 +0530

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread(r'C:\Users\Samhruth\Pictures\Saved Pictures\ForEncry.jpeg')
+img = cv.imread(r'')
 cv.imshow('Sudo', img)
 
 def translate(img, x, y):
